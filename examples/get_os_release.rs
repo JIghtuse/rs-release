422c243f1b9d323b4a7e8ab1d3d538de92e0d905
Boris Egorov <egorov@linux.com>
2016-07-22 10:44:00 +0600

    examples/get_os_release: Use first passed argument as os-release path

--- examples/get_os_release.rs
+++ examples/get_os_release.rs
@@ -1,9 +1,18 @@
 extern crate rs_release;
 
-use rs_release::get_os_release;
+use rs_release::{get_os_release, parse_os_release};
+use std::env;
 
 fn main() {
-    match get_os_release() {
+    let args = env::args();
+
+    let os_release = if let Some(os_release_path) = args.skip(1).next() {
+        parse_os_release(os_release_path)
+    } else {
+        get_os_release()
+    };
+
+    match os_release {
         Ok(os_release) => {
             println!("Parsed os-release:");
             for (k, v) in os_release {
